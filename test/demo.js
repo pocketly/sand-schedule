0838fa7ec10188f37a1d27e8a0333ce0d5dbbae7
Adam Jaso <ajaso@sazze.com>
2015-02-05 20:12:57 -0800

    added lockd support

--- test/demo.js
+++ test/demo.js
@@ -3,6 +3,10 @@
  * @copyright 2015 Pocketly
  */ 
 
+var lockdConfig = {tcp: '127.0.0.1:9999'};
+var server = require('lockd').listen(lockdConfig);
+
 var app = require('sand')({appPath: __dirname, log: '*'})
-  .use(require('..'))
+  .use(require('sand-lockd'), {all: lockdConfig})
+  .use(require('..'), {all: {useSandLockd: true}})
   .start();
--- test/jobs/job.js
+++ test/jobs/job.js
@@ -8,7 +8,8 @@ module.exports = {
   when: {
     second: 30
   },
-  run: function() {
+  run: function(next) {
     console.log('working hard!!!');
+    setTimeout(next, 1000);
   }
 };
